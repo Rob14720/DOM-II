d1c569eb1df1c8ed90705f69b6b819b8e14f03f4
Robert <Roberttsax18@gmail.com>
2023-04-19 12:38:47 -0400

    Getting ideas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './less/index.less'
 
 // Your code goes here!
+console.log('hello');
+
+const heading = document.querySelector('h1')
+heading.textContent = 'Fun Bus!'
+console.log(heading);
+heading.addEventListener('click', (event) =>{
+    event.target.style.color = 'blue'
+})
+
+const nav = document.querySelectorAll('.nav-link')
+console.log(nav);
+nav[0].addEventListener('click', (event) => {
+    event.target.style.color = 'blue'
+})
+nav[1].addEventListener('click', (event) => {
+    event.target.style.color = 'red'
+})
+nav[2].addEventListener('click', (event) => {
+    event.target.style.color = 'yellow'
+})
+nav[3].addEventListener('click', (event) => {
+    event.target.style.color = 'green'
+})
+
+const btns = document.querySelectorAll('.btn')
+console.log(btns)
+btns[0].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'green';
+    });
+    btns[0].addEventListener('click', (event) => {
+        event.target.textContent = 'hooray :)';
+        });
+
+
+
